36df6eb799a9ffc8e984a860c4d311b0f7bd782d
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-10-26 18:10:31 -0700

    allow for heroky build

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Ecommerce
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.react.addons = true
+    config.assets.initialize_on_precompile = false
   end
 end
