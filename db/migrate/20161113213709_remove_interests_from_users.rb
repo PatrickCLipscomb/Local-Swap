b60116fe973ede13159098498704f63e0ed0863b
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-11-15 11:58:05 -0800

    will change paths around to accomodate new requiremnets

--- db/migrate/20161113213709_remove_interests_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInterestsFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :interests, :integer, array: true
-  end
-end
--- db/migrate/20161114222943_drop_users_categories_table.rb
+++
@@ -1,9 +0,0 @@
-class DropUsersCategoriesTable < ActiveRecord::Migration[5.0]
-  def up
-    drop_table :users_categories
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
