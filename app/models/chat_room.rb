c1eeb2dc58fceb2cb452e5d82a52de33d9893dc0
patrick lipscomb <patrick74lipscomb@gmail.com>
2017-01-18 15:37:40 -0800

    configure actioncable for heroku

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,5 +1,5 @@
 class ChatRoom < ApplicationRecord
   belongs_to :user
   has_many :chat_messages, dependent: :destroy
-  validates :title, presence: true, uniqueness: true, case_sensitive: false
+  validates :title, presence: true, case_sensitive: false
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.web_socket_server_url = "wss://localswap-by-patrick-lipscomb.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://localswap-by-patrick-lipscomb.herokuapp.com', 'http://localswap-by-patrick-lipscomb.herokuapp.com']
+  config.web_socket_server_url = "wss://local-swap.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://local-swap.herokuapp.com', 'http://local-swap.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
